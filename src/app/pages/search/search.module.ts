2707027be0d2799d1ab3e23b5f57e1e2ff05d8d8
ComboRush <109625833+DrunkTaric@users.noreply.github.com>
2023-03-06 14:04:05 +0200

    more things for search page

--- src/app/pages/search/search.module.ts
+++ src/app/pages/search/search.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchComponent } from './search.component';
 import { RouterModule } from '@angular/router';
-
-
+import { FormsModule } from '@angular/forms';
+import { MovieService } from '../../services/movie.service';
 
 @NgModule({
   declarations: [
@@ -11,7 +11,12 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    FormsModule
+  ],
+  providers: [
+    MovieService
   ]
 })
+
 export class SearchModule { }
