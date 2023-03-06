6815b3dd759e36c9dc81f99c108318effb3c6732
ComboRush <109625833+DrunkTaric@users.noreply.github.com>
2023-03-06 14:08:11 +0200

    discription
    search function need to be updated dont fucking forget

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,10 +1,29 @@
+import { ITitle } from "../../classes/movier";
+
+import { Router } from "@angular/router";
 import { Component } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { MovieService } from '../../services/movie.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
-export class SearchComponent {
 
+@Injectable()
+export class SearchComponent {
+  constructor (private router: Router, private service: MovieService) {};
+
+  async search(querry: string) {
+    this.router.navigate(['/load'])
+    if (!(querry == "" || querry == " ")) {
+      let response: ITitle[] | null = await this.service.look_for_results(querry);
+      if (response != null) {
+        console.log(response)
+      }else {
+        this.router.navigate(['/error'])
+      }
+    }
+  }
 }
