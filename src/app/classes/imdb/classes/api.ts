1acb6570d6556d7b1b8bf4e053048bcb60754de6
ComboRush <109625833+DrunkTaric@users.noreply.github.com>
2023-03-09 08:20:48 +0200

    discriton
    need a cors key for your shit after 2 days

--- src/app/classes/imdb/classes/api.ts
+++ src/app/classes/imdb/classes/api.ts
@@ -1,7 +1,8 @@
 export class API {
-
     async apiRequestRawHtml(url) {
-      let data = await fetch(`https://api.allorigins.win/raw?url=${encodeURIComponent(url)}`);
+      let data = await fetch(`https://proxy.cors.sh/${url}`, {headers: {
+        'x-cors-api-key': 'temp_6efa2a43278f2a57c3c06e2c628e1a2f',
+      }});
       let text = await data.text();
       return text;
     }
