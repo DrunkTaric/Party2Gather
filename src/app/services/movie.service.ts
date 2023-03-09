45b60383a2e0b3ca9c87de7b1d55cbf68e1e1f54
ComboRush <109625833+DrunkTaric@users.noreply.github.com>
2023-03-09 08:08:10 +0200

    optimizing
    made the info request run on multi-threading to make loading (time of all querries without time)  insted of (wait each querry x time )

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,26 +1,36 @@
 import { Injectable } from '@angular/core';
-import { search, info } from '../classes/imdb/main';
+import { search } from '../classes/imdb/search';
+import { Router } from "@angular/router";
+import { info } from '../classes/imdb/info';
 import bInfo from '../classes/imdb/interfaces/Binfo';
 import dInfo from '../classes/imdb/interfaces/Dinfo';
 
 
 @Injectable()
 export class MovieService {
+
+  constructor(private router: Router) {}
+
   async look_for_results(querry: string): Promise<any> | null {
     let respond: any  = await search(querry)
     if (respond) {
       let _export: dInfo[] = [];
+      let promises: Promise<unknown>[] = []
+      respond.results = respond.results.filter(e => e.type == "movie")
       for (let i = 0; i < respond.results.length; i++) {
         let curr_item: bInfo = respond.results[i]
-        if (curr_item.type == "movie") {
-          let movie_id: string = curr_item.id
-          let data = await info(movie_id)
-          _export.push(data)
-        }
+        let movie_id: string = curr_item.id
+        promises.push(new Promise(reslove => info(movie_id, function(data) {
+          reslove(data)
+        })))
       }
-      return _export;
+      Promise.allSettled(promises).then((values: any) => values.forEach(value => {
+        _export.push(value.value)
+      })).finally(() => {
+        this.router.navigate(['/selector', {data: JSON.stringify(_export)}])
+      });
     }else {
-      return null;
+      this.router.navigate(['/error'])
     }
   }
 }
