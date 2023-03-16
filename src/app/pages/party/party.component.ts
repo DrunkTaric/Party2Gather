eab3504d11e70e8c073249be118e9de5b2395148
ComboRush <109625833+DrunkTaric@users.noreply.github.com>
2023-03-16 23:15:05 +0200

    discription
    setting up the structure for the party page

--- src/app/pages/party/party.component.ts
+++ src/app/pages/party/party.component.ts
@@ -1,3 +1,4 @@
+import Plyr from 'plyr';
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
@@ -8,14 +9,18 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
   styleUrls: ['./party.component.scss']
 })
 export class PartyComponent {
-  link: string
-  safe: SafeResourceUrl
-  constructor(private route: ActivatedRoute, private sanitizer: DomSanitizer) {
-    this.link = `https://www.2embed.to/embed/imdb/movie?id=${this.route.snapshot.paramMap.get("id")}`
+  //link: string
+  //safe: SafeResourceUrl
+  public player: Plyr
+
+  constructor() {
+    //this.link = `https://www.2embed.to/embed/imdb/movie?id=${this.route.snapshot.paramMap.get("id")}`
   }
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-    this.safe = this.sanitizer.bypassSecurityTrustResourceUrl(this.link)
+    //this.safe = this.sanitizer.bypassSecurityTrustResourceUrl(this.link)
+    this.player = new Plyr('#plyrID', {});
+    this.player.once("play", function() {})
   }
 }
